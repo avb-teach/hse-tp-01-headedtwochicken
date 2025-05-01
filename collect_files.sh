9bbcde0f05618af08b9f71ddc13685e956df1b31
chicken2 <mozoleiz0@gmail.com>
2025-05-02 01:22:33 +0300

    added5

--- collect_files.sh
+++ collect_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# источник: https://www.geeksforgeeks.org/how-to-pass-and-parse-linux-bash-script-arguments-and-parameters/  
+# источник парсинга аргументов: https://www.geeksforgeeks.org/how-to-pass-and-parse-linux-bash-script-arguments-and-parameters/  
 if [ "$#" -lt 2 ]; then
     echo "Usage: $0 [--max_depth N] <input_dir> <output_dir>"
     exit 1
@@ -19,11 +19,8 @@ while [ "$#" -gt 0 ]; do
       ;;
   esac
 done
-# источник: https://linuxize.com/post/bash-case-statement/  
-
 input_dir="${args[0]}"
 output_dir="${args[1]}"
-
 
 if [ ! -d "$input_dir" ]; then
     echo "Error: input directory doesn't exist"
@@ -33,27 +30,34 @@ mkdir -p "$output_dir"
 
 rename_if_exists() {
     local fileName="$1" dir="$2" count=1
-    local base="${fileName%.*}" ext="${fileName##*.}" 
+    local base="${fileName%.*}" ext="${fileName##*.}"
     # источник: https://askubuntu.com/questions/538913/how-can-i-copy-files-with-duplicate-filenames-into-one-directory-and-retain-both  
     while [ -e "$dir/$fileName" ]; do
         fileName="${base}${count}.${ext}"
-        count=$((count+1))
+        count=$((count + 1))
     done
     echo "$fileName"
 }
 
-# источник: https://www.baeldung.com/linux/flattening-nested-directory  
 find "$input_dir" -type f | while read -r file; do
-    rel_path="${file#$input_dir/}"
-    dir_path=$(dirname "$rel_path")
-    depth=$(grep -o "/" <<< "$dir_path" | wc -l)
-    if (( depth > max_depth )); then
-        continue
+    rel="${file#$input_dir/}"
+    IFS='/' read -r -a parts <<< "$rel"
+    fileName="${parts[-1]}"
+    dir_count=$(( ${#parts[@]} - 1 ))
+    
+    if (( dir_count == 0 )); then
+        subpath=""
+    elif (( dir_count <= max_depth )); then
+        subpath="${rel%/*}"
+    else
+        start=$(( dir_count - max_depth ))
+        subpath="${parts[@]:start:max_depth}"
+        subpath="${subpath// /\/}"
     fi
 
-    # источник: https://stackoverflow.com/questions/33469292/how-do-i-flatten-a-single-level-of-directories-in-a-shell-command  
-    name=$(basename "$file")
+    dest_dir="$output_dir/$subpath"
+    mkdir -p "$dest_dir"
 
-    newName=$(rename_if_exists "$name" "$output_dir")
-    cp "$file" "$output_dir/$newName"
+    newName=$(rename_if_exists "$fileName" "$dest_dir")
+    cp "$file" "$dest_dir/$newName"
 done
